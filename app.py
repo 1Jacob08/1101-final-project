a37022c235ae4d924b5343b288b9b5c712c54086
Jacob <Jacob.contreras03@utrgv.edu>
2023-12-04 09:23:32 -0600

    updated website

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-#my first comment
-print("hello world")
+
+# Import libraries and packages, they contain additional functions.
+from flask import Flask, render_template
+# Create web application using Flask
+app = Flask(__name__)
+# Create our routes
+@app.route('/')
+def home():
+ return render_template('index.html')
+# Start the web application
+app.run(
+ debug=True
+)
